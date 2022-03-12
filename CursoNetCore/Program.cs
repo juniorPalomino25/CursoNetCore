3414fd2b30b2624a8e89af4fca2f5f71371a2c37
juniorPalomino25 <Junior.palomino@unmsm.edu.pe>
2022-03-12 12:12:42 -0500

    tipo de variables

--- CursoNetCore/Program.cs
+++ CursoNetCore/Program.cs
@@ -6,7 +6,28 @@ namespace CursoNetCore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //tipo de variables
+            int year = 22; //entero
+            float flotante = 13.5f; //float 7 dígitos -> 32 bits
+            double doble = 12.004; //15 - 16 dígitos  -> 64 bits
+            decimal decimals = 15.0000342M; // 28 - 29 digicots -> 128 bits
+
+            // cadenas y caracteres
+            char letra = 'a'; //caracter
+            string nombre = "junior palomino";
+
+            //valores booleanos
+            bool valor = true;
+
+            //instanciando algunos objetos
+            String apellidos = "palomino loa";
+
+            //instanciando estructuras
+            Decimal dec = 24.00928M;
+            Char sexo = 'M';
+            Int16 ciclo = 9;
+
+            Console.WriteLine("Hello World! {0} {1} ", year, ciclo);
             Console.ReadLine();
         }
     }
