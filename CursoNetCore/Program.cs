a4c94ab5a311e1c6235c0838997c9f4b1367c298
juniorPalomino25 <Junior.palomino@unmsm.edu.pe>
2022-03-12 12:54:56 -0500

    adicion de los demas operadores

--- CursoNetCore/Program.cs
+++ CursoNetCore/Program.cs
@@ -6,9 +6,9 @@ namespace CursoNetCore
     {
         static void Main(string[] args)
         {
-            /*
             
-             * //tipo de variables
+            
+             //tipo de variables
 
             //valores numericos
             int year = 22; //entero
@@ -18,7 +18,7 @@ namespace CursoNetCore
 
             // cadenas y caracteres
             char letra = 'a'; //caracter
-            string nombre = "junior palomino";
+            string nombre = "junior jose";
 
             //valores booleanos
             bool valor = true;
@@ -31,11 +31,8 @@ namespace CursoNetCore
             Char sexo = 'M';
             Int16 ciclo = 9;
 
-            Console.WriteLine("Hello World! {0} {1} ", year, ciclo);
-            Console.ReadLine();
-
-             */
-
+            Console.WriteLine("Hello " + nombre + " " +apellidos + " {0} {1} {2} {3} {4} {5}", year, flotante, sexo, dec, apellidos,nombre + "\n");
+            
             // Operadores
 
             //operador aritmético
@@ -47,10 +44,30 @@ namespace CursoNetCore
             var div = valor1 / valor2;
 
             Console.WriteLine("Operadores matemáticos \n");
+            Console.WriteLine("valor 1: "+valor1 + " y valor 2: " +valor2 + "\n");
             Console.WriteLine("la suma es: " + suma + "\n");
             Console.WriteLine("la resta es: " + resta + "\n");
             Console.WriteLine("la multiplicacion es: " + mult + "\n");
-            Console.WriteLine("la division es: " + div + "\n");
+            Console.WriteLine("la division es: " + div + "\n"); 
+
+            //operador asignacion
+            var aux = valor1;
+            valor1 = valor2;
+            valor2 = aux;
+            Console.WriteLine("nuevo valor 1: " + valor1 + "\n");
+            Console.WriteLine("nuevo valor 2: " + valor2 + "\n");
+
+            //operador relacional
+            var bandera = 5 < 2;
+            Console.WriteLine("5<2 ? : " + bandera);
+
+            //operador de igualdad
+            var esIgual = 5 == 2;
+            var esDiferente = 5 != 2;
+            Console.WriteLine("5 = 2 ? : " + esIgual);
+            Console.WriteLine("5 <> 2 ? : " + esDiferente);
+
+
             Console.ReadLine();
         }
     }
